b6d168e0569662747180769d94f7fa93c74d8a1b
ck-linuxx <amorimclark@gmail.com>
2021-09-01 20:44:42 -0300

    master

---
+++ main.js
@@ -0,0 +1,55 @@
+window.onload = function (){
+    var hour = 00
+    var minutes = 00
+    var seconds = 00
+    var tens = 00
+    var idHour = document.getElementById("hour")
+    var idMinutes = document.getElementById("minutes")
+    var idSeconds = document.getElementById("seconds")
+    var start = document.getElementById("start")
+    var pause = document.getElementById("pause")
+    var reset = document.getElementById("reset")
+    var interval;
+    
+    start.onclick = function() {
+        clearInterval(interval);
+        interval = setInterval(startTimer, 10)
+    }
+
+    pause.onclick = function() {
+        clearInterval(interval)
+    }
+
+    reset.onclick = function() {
+        clearInterval(interval)
+        tens = "00"
+        seconds = "00"
+        minutes = "00"
+        hour = "00"
+        idSeconds.innerHTML = seconds
+        idMinutes.innerHTML = minutes
+        idHourinnerHTML = hour
+    }
+
+    function startTimer(){
+        tens++
+
+        if(tens > 99){
+            console.log("seconds")
+            seconds++
+            idSeconds.innerHTML = "0" + seconds
+            tens = 0
+        }
+
+        if(seconds > 9){
+            idSeconds.innerHTML = seconds
+        }
+
+        if(seconds == 59){
+            seconds = 00
+            minutes++
+            idMinutes.innerHTML = "0" + minutes
+        }
+    }
+
+}
